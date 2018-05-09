9f99bd42c8ef74012742e97780f5b8da511a0fe1
HauptmannEck <eckhoffj@gmail.com>
2018-05-08 22:09:53 -0500

    Add basic bingo board layout

--- client/src/components/app/index.jsx
+++ client/src/components/app/index.jsx
@@ -4,9 +4,40 @@ import Toolbar from 'material-ui/Toolbar';
 import IconButton from 'material-ui/IconButton';
 import Icon from 'material-ui/Icon';
 import Typography from 'material-ui/Typography';
-import Grid from 'material-ui/Grid';
+import Paper from 'material-ui/Paper';
 import styles from './styles.less';
-import JSS from "../jss";
+import JSS from '../jss';
+
+const boxes = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25];
+
+const Board = ({boxes}) => {
+	return (
+		<div className={styles.board}>
+			<Typography variant="title" color="inherit" align="center">
+				B
+			</Typography>
+			<Typography variant="title" color="inherit" align="center">
+				I
+			</Typography>
+			<Typography variant="title" color="inherit" align="center">
+				N
+			</Typography>
+			<Typography variant="title" color="inherit" align="center">
+				G
+			</Typography>
+			<Typography variant="title" color="inherit" align="center">
+				O
+			</Typography>
+			{boxes.map((item) => (
+				<Paper key={item} className={styles.box}>
+					<div className={styles.content}>
+						{item}
+					</div>
+				</Paper>
+			))}
+		</div>
+	);
+};
 
 class App extends Component {
 	render() {
@@ -26,17 +57,12 @@ class App extends Component {
 							</IconButton>
 						</Toolbar>
 					</AppBar>
-					<Grid container spacing={16} className={styles.root}>
-						<Grid item xs={6}>
-							LEFT
-						</Grid>
-						<Grid item xs={6}>
-							Right
-						</Grid>
-						<Grid item xs={12} style={{textAlign: 'center'}}>
-							Bingo Time is mine not yours 2018
-						</Grid>
-					</Grid>
+					<div className={styles.root}>
+						<Board boxes={boxes}/>
+					</div>
+					<div className={styles.footer}>
+						Bingo Time is mine not yours 2018
+					</div>
 				</Fragment>
 			</JSS>
 		);
